5e32195b2f795fc4a05febea6a283d1c2f102841
saikumar <sai680513@gmail.com>
2025-03-31 10:13:01 +0530

    updated new_therapy.py file

--- new_therapy.py
+++ new_therapy.py
@@ -1665,4 +1665,4 @@ def serve_generated_music(filename):
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000)) 
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
